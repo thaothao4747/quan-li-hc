9466562aa0b4dad2d09ed2f0c90d58bfe79e05f1
thaothao4747 <thaothao4747@gmail.com>
2026-07-03 14:17:55 +0700

    Add files via upload

---
+++ NETLIFY-UPLOAD-QUAN-LI-HC/sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='qlhc-20260703-auto-1';
+const FILES=['./','./index.html','./manifest.webmanifest','./app-icon-coffee.png','./version.json'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request))) });
