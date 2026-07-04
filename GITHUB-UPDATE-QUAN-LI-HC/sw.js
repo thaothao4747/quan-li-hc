25be03cbcf95b63957ed27ed7b955822d16c22dd
thaothao4747 <thaothao4747@gmail.com>
2026-07-04 18:04:15 +0700

    Add files via upload

--- GITHUB-UPDATE-QUAN-LI-HC/sw.js
+++ GITHUB-UPDATE-QUAN-LI-HC/sw.js
@@ -1,5 +1,5 @@
-﻿const CACHE_NAME = 'qlhc-20260704-report-compact-layout-1';
-const FILES = ['./','./index.html?v=20260704-report-compact-layout-1','./quan-ly-kho-cong-cu.html?v=20260704-report-compact-layout-1','./manifest.webmanifest?v=20260704-report-compact-layout-1','./app-icon-coffee.png?v=20260704-report-compact-layout-1','./icon-192.png?v=20260704-report-compact-layout-1','./icon-512.png?v=20260704-report-compact-layout-1','./version.json?v=20260704-report-compact-layout-1'];
+﻿const CACHE_NAME = 'qlhc-20260704-login-coffee-fresh-1';
+const FILES = ['./','./index.html?v=20260704-login-coffee-fresh-1','./quan-ly-kho-cong-cu.html?v=20260704-login-coffee-fresh-1','./manifest.webmanifest?v=20260704-login-coffee-fresh-1','./app-icon-coffee.png?v=20260704-login-coffee-fresh-1','./login-coffee-bg.png?v=20260704-login-coffee-fresh-1','./icon-192.png?v=20260704-login-coffee-fresh-1','./icon-512.png?v=20260704-login-coffee-fresh-1','./version.json?v=20260704-login-coffee-fresh-1'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))); });
