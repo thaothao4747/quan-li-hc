473333bce68c7c1ca72eca68138beb9afb19400b
thaothao4747 <thaothao4747@gmail.com>
2026-07-04 14:49:27 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-﻿const CACHE_NAME = 'qlhc-20260704-compact-soft-ui-1';
+﻿const CACHE_NAME = 'qlhc-20260704-reset-stock-start-2';
 const FILES = ['./index.html', './quan-ly-kho-cong-cu.html', './manifest.webmanifest', './app-icon-coffee.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => { event.respondWith(fetch(event.request, {cache:'no-store'}).catch(() => caches.match(event.request))); });
+
+
 
 
 
