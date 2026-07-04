593f9dc736bec6bf44121c9bfde0654fd991cf1f
thaothao4747 <thaothao4747@gmail.com>
2026-07-04 17:38:31 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,52 +1,5 @@
-﻿const CACHE_NAME = 'qlhc-20260704-ton-search-stock-only-1';
-const FILES = ['./index.html', './quan-ly-kho-cong-cu.html', './manifest.webmanifest', './app-icon-coffee.png'];
+﻿const CACHE_NAME = 'qlhc-20260704-report-search-edit-sync-1';
+const FILES = ['./','./index.html?v=20260704-report-search-edit-sync-1','./quan-ly-kho-cong-cu.html?v=20260704-report-search-edit-sync-1','./manifest.webmanifest?v=20260704-report-search-edit-sync-1','./app-icon-coffee.png?v=20260704-report-search-edit-sync-1','./icon-192.png?v=20260704-report-search-edit-sync-1','./icon-512.png?v=20260704-report-search-edit-sync-1','./version.json?v=20260704-report-search-edit-sync-1'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request, {cache:'no-store'}).catch(() => caches.match(event.request))); });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))); });
