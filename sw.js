37a046fc7172becdb159665666b46904dfa63729
thaothao4747 <thaothao4747@gmail.com>
2026-07-06 10:36:49 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-﻿const CACHE_NAME = 'qlhc-20260706-robust-user-login-1';
-const FILES = ['./','./index.html?v=20260706-robust-user-login-1','./quan-ly-kho-cong-cu.html?v=20260706-robust-user-login-1','./manifest.webmanifest?v=20260706-robust-user-login-1','./app-icon-coffee.png?v=20260706-robust-user-login-1','./login-coffee-bg.png?v=20260706-robust-user-login-1','./icon-192.png?v=20260706-robust-user-login-1','./icon-512.png?v=20260706-robust-user-login-1','./version.json?v=20260706-robust-user-login-1'];
+﻿const CACHE_NAME = 'qlhc-20260706-mobile-cache-login-fix-1';
+const FILES = ['./','./index.html?v=20260706-mobile-cache-login-fix-1','./quan-ly-kho-cong-cu.html?v=20260706-mobile-cache-login-fix-1','./manifest.webmanifest?v=20260706-mobile-cache-login-fix-1','./app-icon-coffee.png?v=20260706-mobile-cache-login-fix-1','./login-coffee-bg.png?v=20260706-mobile-cache-login-fix-1','./icon-192.png?v=20260706-mobile-cache-login-fix-1','./icon-512.png?v=20260706-mobile-cache-login-fix-1','./version.json?v=20260706-mobile-cache-login-fix-1'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))); });
+
 
 
 
