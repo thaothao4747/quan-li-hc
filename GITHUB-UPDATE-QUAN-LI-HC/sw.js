6fefdc0bff54a919babb02ee7393075e29a86cbe
thaothao4747 <thaothao4747@gmail.com>
2026-07-06 10:19:14 +0700

    Add files via upload

--- GITHUB-UPDATE-QUAN-LI-HC/sw.js
+++ GITHUB-UPDATE-QUAN-LI-HC/sw.js
@@ -1,8 +1,10 @@
-﻿const CACHE_NAME = 'qlhc-20260706-cap-phat-nhan-vien-bo-phan-1';
-const FILES = ['./','./index.html?v=20260706-cap-phat-nhan-vien-bo-phan-1','./quan-ly-kho-cong-cu.html?v=20260706-cap-phat-nhan-vien-bo-phan-1','./manifest.webmanifest?v=20260706-cap-phat-nhan-vien-bo-phan-1','./app-icon-coffee.png?v=20260706-cap-phat-nhan-vien-bo-phan-1','./login-coffee-bg.png?v=20260706-cap-phat-nhan-vien-bo-phan-1','./icon-192.png?v=20260706-cap-phat-nhan-vien-bo-phan-1','./icon-512.png?v=20260706-cap-phat-nhan-vien-bo-phan-1','./version.json?v=20260706-cap-phat-nhan-vien-bo-phan-1'];
+﻿const CACHE_NAME = 'qlhc-20260706-restore-user-permission-settings-1';
+const FILES = ['./','./index.html?v=20260706-restore-user-permission-settings-1','./quan-ly-kho-cong-cu.html?v=20260706-restore-user-permission-settings-1','./manifest.webmanifest?v=20260706-restore-user-permission-settings-1','./app-icon-coffee.png?v=20260706-restore-user-permission-settings-1','./login-coffee-bg.png?v=20260706-restore-user-permission-settings-1','./icon-192.png?v=20260706-restore-user-permission-settings-1','./icon-512.png?v=20260706-restore-user-permission-settings-1','./version.json?v=20260706-restore-user-permission-settings-1'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))); });
+
+
 
 
 
