93fcd6904fb9050ca9b1fad4939c3938125a76fe
thaothao4747 <thaothao4747@gmail.com>
2026-07-07 15:05:25 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'qlhc-20260707-overview-stock-search-1';
+const CACHE_NAME = 'qlhc-20260707-overview-search-clean-1';
 const ASSETS = [
   './',
-  './index.html?v=20260707-overview-stock-search-1',
-  './quan-ly-kho-cong-cu.html?v=20260707-overview-stock-search-1',
-  './mobile-app.html?v=20260707-overview-stock-search-1',
-  './manifest.webmanifest?v=20260707-overview-stock-search-1',
+  './index.html?v=20260707-overview-search-clean-1',
+  './quan-ly-kho-cong-cu.html?v=20260707-overview-search-clean-1',
+  './mobile-app.html?v=20260707-overview-search-clean-1',
+  './manifest.webmanifest?v=20260707-overview-search-clean-1',
   './app-icon-coffee.png',
   './icon-192.png',
   './icon-512.png'
