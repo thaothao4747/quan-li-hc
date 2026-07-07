a8b70a01792a2f3aca62beb82e4e712e299af2b6
thaothao4747 <thaothao4747@gmail.com>
2026-07-07 13:42:00 +0700

    Add files via upload

--- GITHUB-UPDATE-QUAN-LI-HC/sw.js
+++ GITHUB-UPDATE-QUAN-LI-HC/sw.js
@@ -1,29 +1,28 @@
-﻿const CACHE_NAME = 'qlhc-20260707-group-field-report-1';
-const APP_SHELL = [
+const CACHE_NAME = 'qlhc-20260707-overview-stock-search-1';
+const ASSETS = [
   './',
-  './index.html',
-  './quan-ly-kho-cong-cu.html',
-  './mobile-app.html',
-  './404.html',
-  './manifest.webmanifest',
+  './index.html?v=20260707-overview-stock-search-1',
+  './quan-ly-kho-cong-cu.html?v=20260707-overview-stock-search-1',
+  './mobile-app.html?v=20260707-overview-stock-search-1',
+  './manifest.webmanifest?v=20260707-overview-stock-search-1',
   './app-icon-coffee.png',
   './icon-192.png',
-  './icon-512.png',
-  './login-coffee-bg.png',
-  './version.json'
+  './icon-512.png'
 ];
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).catch(() => undefined));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null));
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('qlhc-') && key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return;
   event.respondWith(fetch(event.request).then(response => {
     const copy = response.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => undefined);
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => null);
     return response;
   }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))));
 });
