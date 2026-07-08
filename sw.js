e52b5cfd5db557b0f6ded32d9bcf6235dc1e471a
thaothao4747 <thaothao4747@gmail.com>
2026-07-08 09:31:10 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'qlhc-20260708-hide-inline-detail-label-1';
+const CACHE_NAME = 'qlhc-20260708-inline-clear-search-button-1';
 const ASSETS = [
   './',
-  './index.html?v=20260708-hide-inline-detail-label-1',
-  './quan-ly-kho-cong-cu.html?v=20260708-hide-inline-detail-label-1',
-  './mobile-app.html?v=20260708-hide-inline-detail-label-1',
-  './manifest.webmanifest?v=20260708-hide-inline-detail-label-1',
+  './index.html?v=20260708-inline-clear-search-button-1',
+  './quan-ly-kho-cong-cu.html?v=20260708-inline-clear-search-button-1',
+  './mobile-app.html?v=20260708-inline-clear-search-button-1',
+  './manifest.webmanifest?v=20260708-inline-clear-search-button-1',
   './app-icon-coffee.png',
   './icon-192.png',
   './icon-512.png'
